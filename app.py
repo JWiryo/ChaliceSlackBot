1485dd62a807a7f1ce5798f857e6b3574e345eea
James Wiryo <jamesgiggs95@gmail.com>
2018-08-24 01:16:36 +0800

    Deployed working Slackbot code to Lambda Prod

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from chalice import Chalice
 from datetime import datetime
 
+import os
+import requests
+
 import json
 
 app = Chalice(app_name='slackBotWines')
 app.debug = True
 
+slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
+
 @app.route('/wines')
 def notify_wines():
     currentDate = datetime.now().strftime("%Y-%m-%d")
     message = "Today is %s; Please remember to input your WINES for this week" % currentDate
+    response = requests.post(slack_webhook_url, data=json.dumps({'text': message}))
     return {'message' : json.dumps(message)}
