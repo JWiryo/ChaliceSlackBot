52d3dcb43fb1f5dafd0859b4f83df434ef841580
James Wiryo <jamesgiggs95@gmail.com>
2018-08-27 01:37:39 +0800

    Created Scheduled CloudWatch Event to trigger Lambda Function to notify Slack users on DishDash orders

--- app.py
+++ app.py
@@ -11,9 +11,17 @@ app.debug = True
 
 slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
 
+
 @app.route('/wines')
 def notify_wines():
     currentDate = datetime.now().strftime("%Y-%m-%d")
     message = "Today is %s; Please remember to input your WINES for this week" % currentDate
     response = requests.post(slack_webhook_url, data=json.dumps({'text': message}))
-    return {'message' : json.dumps(message)}
+    return {'message': json.dumps(message)}
+
+
+@app.schedule('cron(30 1 ? * MON *)')
+def notify_dishdash(event):
+    message = "Hi Guys! Don't forget to order next week's DishDash! https://www.dishdash.co/ "
+    response = requests.post(slack_webhook_url, data=json.dumps({'text': message}))
+    return {'message': json.dumps(message)}
